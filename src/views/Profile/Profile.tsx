98a8c900b0d5b1ee01d91655d9def51f49829163
Corliansa Kusumah <corliansa@gmail.com>
2022-11-26 23:27:21 +0100

    added user info on profile

--- src/views/Profile/Profile.tsx
+++ src/views/Profile/Profile.tsx
@@ -20,7 +20,41 @@ export const Profile: NextPage = () => {
           </h1>
           {!isLoading && user && (
             <Protected>
-              <Form user={user} />
+              <div className="grid grid-cols-1 gap-4 md:grid-cols-2">
+                <Card key={user.id}>
+                  <h2 className="text-2xl font-bold">{user.name}</h2>
+                  {[
+                    { label: "Email", value: user.email },
+                    { label: "Role", value: user.role },
+                    { label: "Status", value: user.status },
+                    {
+                      label: "Verified",
+                      value: user.verification === "VERIFIED" ? "Yes" : "No",
+                    },
+                    {
+                      label: "Birth date",
+                      value: user.birthDate?.toLocaleDateString() ?? "N/A",
+                    },
+                    { label: "Occupation", value: user.occupation ?? "N/A" },
+                    {
+                      label: "Affiliation",
+                      value:
+                        user.affiliation.length > 0
+                          ? user.affiliation.join(" ,")
+                          : "N/A",
+                    },
+                    { label: "City", value: user.city ?? "N/A" },
+                  ].map(({ label, value }) => (
+                    <p key={label}>
+                      <>
+                        <span className="font-bold">{label}: </span>
+                        {value}
+                      </>
+                    </p>
+                  ))}
+                </Card>
+                <Form user={user} />
+              </div>
             </Protected>
           )}
         </Container>
@@ -48,8 +82,11 @@ export const Form: React.FC<{
   const [occupation, setOccupation] = useState(user?.occupation ?? "");
   const [city, setCity] = useState(user?.city ?? "");
 
+  const queryClient = trpc.useContext();
   const { mutateAsync: updateUser, isLoading } =
-    trpc.user.updateUser.useMutation();
+    trpc.user.updateUser.useMutation({
+      onSuccess: () => queryClient.user.getUser.invalidate(),
+    });
   return (
     <Card>
       Name
