e864a1883999c9816d90a5d142358664827acd1f
Corliansa Kusumah <corliansa@gmail.com>
2022-11-30 16:51:47 +0100

    updated schema, protected and added hook

--- src/Components/Protected.tsx
+++ src/Components/Protected.tsx
@@ -1,26 +1,32 @@
 import { useSession } from "next-auth/react";
+import { useRouter } from "next/router";
 import type { RoleType } from "../types";
 
 export const Protected: React.FC<{
   children: React.ReactNode;
   roles?: RoleType[];
   hideIfNotAuthorized?: boolean;
+  redirectTo?: string;
 }> = (props) => {
   const { data: session, status } = useSession();
+  const { replace } = useRouter();
 
   if (status === "loading") {
     return props?.hideIfNotAuthorized ? null : <p>Loading...</p>;
   }
 
   if (status === "unauthenticated") {
+    props.redirectTo && replace(props.redirectTo || "/");
     return props?.hideIfNotAuthorized ? null : <p>Unauthenticated</p>;
   }
 
   if (!session || !session.user) {
+    props.redirectTo && replace(props.redirectTo || "/");
     return props?.hideIfNotAuthorized ? null : <p>Unauthorized</p>;
   }
 
   if (props.roles && !props.roles.includes(session.user.role as RoleType)) {
+    props.redirectTo && replace(props.redirectTo || "/");
     return props?.hideIfNotAuthorized ? null : <p>Forbidden</p>;
   }
 
