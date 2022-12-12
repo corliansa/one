51dc8e00a4f3a64e4f4609de10a14cd9b95a75b7
Corliansa Kusumah <corliansa@gmail.com>
2022-12-12 16:58:17 +0100

    Styling fix

--- src/Components/Logo.tsx
+++ src/Components/Logo.tsx
@@ -2,7 +2,7 @@ export const Logo = () => (
   // eslint-disable-next-line @next/next/no-img-element
   <img
     className="h-8"
-    src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
+    src="https://tailwindui.com/img/logos/mark.svg?color=pink&shade=600"
     alt=""
   />
 );
--- src/views/Home/Home.tsx
+++ src/views/Home/Home.tsx
@@ -154,7 +154,7 @@ export const Home = () => {
                 <div className="relative overflow-hidden rounded-full py-1.5 px-4 text-sm leading-6 ring-1 ring-gray-900/10 hover:ring-gray-900/20">
                   <span className="text-gray-600">
                     Announcing our latest features.{" "}
-                    <a href="#" className="font-semibold text-indigo-600">
+                    <a href="#" className="font-semibold text-pink-600">
                       <span className="absolute inset-0" aria-hidden="true" />
                       Read more <span aria-hidden="true">&rarr;</span>
                     </a>
@@ -163,20 +163,29 @@ export const Home = () => {
               </div> */}
                 <div>
                   <h1 className="text-4xl font-bold tracking-tight sm:text-center sm:text-6xl">
-                    Connect with others, easily
+                    Connecting people, reimagined.
                   </h1>
                   <p className="mt-6 text-lg leading-8 text-gray-600 sm:text-center">
-                    Experience a new way to connect with others.
+                    Experience how ONE can help you connect with others.
                   </p>
                   <div className="mt-8 flex gap-x-4 sm:justify-center">
                     <a
                       onClick={() =>
                         sessionData ? push("/dashboard") : signIn("google")
                       }
-                      className="inline-block cursor-pointer rounded-lg bg-indigo-600 px-4 py-1.5 text-base font-semibold leading-7 text-white shadow-sm ring-1 ring-indigo-600 hover:bg-indigo-700 hover:ring-indigo-700"
+                      className="inline-block cursor-pointer rounded-lg bg-pink-600 px-4 py-1.5 text-base font-semibold leading-7 text-white shadow-sm ring-1 ring-pink-600 hover:bg-pink-700 hover:ring-pink-700"
                     >
-                      Get started
-                      <span className="text-indigo-200" aria-hidden="true">
+                      Get started{" "}
+                      <span className="text-pink-200" aria-hidden="true">
+                        &rarr;
+                      </span>
+                    </a>
+                    <a
+                      href="#"
+                      className="inline-block rounded-lg px-4 py-1.5 text-base font-semibold leading-7 text-gray-900 ring-1 ring-gray-900/10 hover:ring-gray-900/20"
+                    >
+                      Learn more{" "}
+                      <span className="text-gray-400" aria-hidden="true">
                         &rarr;
                       </span>
                     </a>
