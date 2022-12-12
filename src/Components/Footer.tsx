5be67945293bba81d0e10ec69b86c26db5a53680
Corliansa Kusumah <corliansa@gmail.com>
2022-12-12 22:06:25 +0100

    Styling fix

--- src/Components/Footer.tsx
+++ src/Components/Footer.tsx
@@ -52,7 +52,7 @@ const navigation: {
 
 export const Footer: React.FC = () => {
   return (
-    <footer className="w-full bg-white">
+    <footer className="bg-white">
       <div className="mx-auto max-w-7xl overflow-hidden py-12 px-4 sm:px-6 lg:px-8">
         <nav
           className="-mx-5 -my-2 flex flex-wrap justify-center"
@@ -62,7 +62,7 @@ export const Footer: React.FC = () => {
             <div key={item.name} className="px-5 py-2">
               <a
                 href={item.href}
-                className="text-base text-gray-500 hover:text-gray-900"
+                className="cursor-pointer text-base text-gray-500 hover:text-gray-900"
               >
                 {item.name}
               </a>
@@ -74,7 +74,7 @@ export const Footer: React.FC = () => {
             <a
               key={item.name}
               href={item.href}
-              className="text-gray-400 hover:text-gray-500"
+              className="cursor-pointer text-gray-400 hover:text-gray-500"
             >
               <span className="sr-only">{item.name}</span>
               <item.icon className="h-6 w-6" aria-hidden="true" />
--- src/views/Home/Home.tsx
+++ src/views/Home/Home.tsx
@@ -193,6 +193,7 @@ export const Home = () => {
                     </a>
                   </div>
                 </div>
+                <Footer />
                 <div className="absolute inset-x-0 top-[calc(100%-13rem)] -z-10 transform-gpu overflow-hidden blur-3xl sm:top-[calc(100%-30rem)]">
                   <svg
                     className="relative left-[calc(50%+3rem)] h-[21.1875rem] max-w-none -translate-x-1/2 sm:left-[calc(50%+36rem)] sm:h-[42.375rem]"
@@ -225,7 +226,6 @@ export const Home = () => {
           </div>
         </main>
       </div>
-      <Footer />
     </>
   );
 };
