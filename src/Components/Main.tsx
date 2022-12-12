7439770b2b30b124b6407b40118caff08f6964ee
Corliansa Kusumah <corliansa@gmail.com>
2022-12-12 16:42:51 +0100

    Removed obsolete component

--- src/Components/Main.tsx
+++
@@ -1,6 +0,0 @@
-export const Main: React.FC<{ children: React.ReactNode }> = (props) => (
-  <main
-    className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#022f78] to-[#15162c] text-white"
-    {...props}
-  />
-);
