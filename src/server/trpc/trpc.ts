2eec5c5361e50f568f5e173f75e93669ccb03ce3
Azhar Rahadian <a.rahadian@tu-braunschweig.de>
2024-03-11 18:07:51 +0100

    ini kenapa ya kok changes nya ke redo anjay

--- src/server/trpc/trpc.ts
+++ src/server/trpc/trpc.ts
@@ -16,10 +16,10 @@ export const router = t.router;
  * Unprotected procedure
  **/
 export const publicProcedure = t.procedure;
-export const protectedProcedure = t.procedure;
 /**
  * Reusable middleware to ensure
  * users are logged in
+ * */
 
 const isAuthed = t.middleware(async ({ ctx, next, path, input }) => {
   if (!ctx.session || !ctx.session.user) {
@@ -34,7 +34,7 @@ const isAuthed = t.middleware(async ({ ctx, next, path, input }) => {
     },
   });
   if (!audit.id) {
-    console.warn("Failed to audit request")
+    console.warn("Failed to audit request");
     throw new TRPCError({
       code: "INTERNAL_SERVER_ERROR",
       message: "Failed to audit request",
@@ -47,19 +47,17 @@ const isAuthed = t.middleware(async ({ ctx, next, path, input }) => {
     status: "ACTIVE",
     verification: "VERIFIED",
     name: "test name",
-    email: "test@gmail.com"
-}
+    email: "test@gmail.com",
+  };
   return next({
     ctx: {
       // infers the `session` as non-nullable
-      session: {user},
+      session: { user },
       auditId: audit.id,
     },
   });
 });
 
-/**
- * Protected procedure
+// Protected procedure
 
 export const protectedProcedure = t.procedure.use(isAuthed);
- **/
