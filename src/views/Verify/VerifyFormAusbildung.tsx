97e5ae32f2b90f172bc24255e8602d7449c326f0
Azhar Rahadian <a.rahadian@tu-braunschweig.de>
2024-04-06 10:59:04 +0200

    Small CHange

--- src/views/Verify/VerifyFormAusbildung.tsx
+++ src/views/Verify/VerifyFormAusbildung.tsx
@@ -1,3 +1,8 @@
 export const VerifyFormAusbildung: React.FC = () => {
-  return <h1>VerifyFormAusbildung</h1>;
+  return (
+    <h1>
+      Untuk verifikasi Ausbildung, kami sedang mencari metode yang pas untuk
+      verifikasi. Mohon maaf atas ketidaknyamanannya.
+    </h1>
+  );
 };
